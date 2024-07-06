30284813f257e70dd8d4a98be1c869ed3621588b
Ferhat <89192603+Doubasz@users.noreply.github.com>
2024-07-06 21:35:50 +0100

    Update Main.java

--- Chess/src/main/java/org/example/chess/Main.java
+++ Chess/src/main/java/org/example/chess/Main.java
@@ -25,66 +25,6 @@ public class Main extends Application {
         stage.show();
     }
 
-    /*public void makingMoves(){
-        Button temp = new Button();
-        temp.setGraphic(gridGame[4][1].getGraphic());
-        gridGame[4][1].setGraphic(gridGame[4][3].getGraphic());
-        gridGame[4][3].setGraphic(temp.getGraphic());
-    }*/
-
-    /*public void innitPieces() throws NullPointerException{
-
-        //Rooks
-        ImageView blackRook = new ImageView(getClass().getResource("img/blackRook.png").toExternalForm());
-        ImageView blackRook1 = new ImageView(getClass().getResource("img/blackRook.png").toExternalForm());
-
-        gridGame[0][0].setGraphic(blackRook);
-        gridGame[7][0].setGraphic(blackRook1);
-
-        //Bishops
-        ImageView blackBishop = new ImageView(getClass().getResource("img/blackBishop.png").toExternalForm());
-        ImageView blackBishop1 = new ImageView(getClass().getResource("img/blackBishop.png").toExternalForm());
-
-        gridGame[2][0].setGraphic(blackBishop);
-        gridGame[5][0].setGraphic(blackBishop1);
-
-        //Knights
-        ImageView blackKnight = new ImageView(getClass().getResource("img/blackKnight.png").toExternalForm());
-        ImageView blackKnight1 = new ImageView(getClass().getResource("img/blackKnight.png").toExternalForm());
-
-        gridGame[1][0].setGraphic(blackKnight);
-        gridGame[6][0].setGraphic(blackKnight1);
-
-        //Queens
-        ImageView blackQueen = new ImageView(getClass().getResource("img/blackQueen.png").toExternalForm());
-
-        gridGame[3][0].setGraphic(blackQueen);
-
-        //Kings
-        ImageView blackKing = new ImageView(getClass().getResource("img/blackKing.png").toExternalForm());
-
-        gridGame[4][0].setGraphic(blackKing);
-
-        //Pawns
-        ImageView blackPawn = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn1 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn2 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn3 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn4 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn5 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn6 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-        ImageView blackPawn7 = new ImageView(getClass().getResource("img/blackPawn.png").toExternalForm());
-
-        gridGame[0][1].setGraphic(blackPawn);
-        gridGame[1][1].setGraphic(blackPawn1);
-        gridGame[2][1].setGraphic(blackPawn2);
-        gridGame[3][1].setGraphic(blackPawn3);
-        gridGame[4][1].setGraphic(blackPawn4);
-        gridGame[5][1].setGraphic(blackPawn5);
-        gridGame[6][1].setGraphic(blackPawn6);
-        gridGame[7][1].setGraphic(blackPawn7);
-    }*/
-
     public static void main(String[] args) {
         launch();
     }
